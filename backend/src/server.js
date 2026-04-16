6f3a8f556233c14c9efae6b7a98e502043eab157
alphaMale28 <manandharsanil007@gmail.com>
2026-04-15 22:26:59 -0400

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,26 @@
 import express from "express";
+import path from "path";
 
 import { ENV } from "./lib/env.js";
 import authRouters from "./routers/auth.router.js";
 import messageRouters from "./routers/message.router.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 8000;
 
 app.use("/api/auth", authRouters);
 app.use("/api/message", messageRouters);
 
-app.listen(PORT, () => console.log("Server running in port:", PORT));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    return res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log("Server running in port:", PORT);
+});
