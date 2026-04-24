6bb98cdc2e339369a6b258a7519e038b45663d98
alphaMale28 <manandharsanil007@gmail.com>
2026-04-24 06:34:09 -0400

    feat: implemented rate limiting

--- backend/src/routers/auth.router.js
+++ backend/src/routers/auth.router.js
@@ -8,8 +8,11 @@ import {
   updateProfile,
 } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.use(arcjetProtection);
 
 router.get("/check-email", checkEmail);
 
